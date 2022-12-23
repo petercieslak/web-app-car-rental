684bab6dd1d5c54d4fb34985d32c87863eafbf16
piotrc <piotr.m.cieslak@gmail.com>
2022-12-23 22:46:32 +0100

    Model config

--- car_rental/rental/models.py
+++ car_rental/rental/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class CarType(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.ForeignKey(CarType, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='car_images')
+
+
+class Rental(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    def __str__(self):
+        return self.car.name
